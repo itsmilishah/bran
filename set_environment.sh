cd878ff877ec0df7730caa9d1baf5b1582801c6b
Mili Shah <mashah@gypsum.cm.cluster>
2018-03-30 11:01:22 -0400

    Changes to setup environment

--- set_environment.sh
+++ set_environment.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+# export input_dir=${CDR_IE_ROOT}/data/cdr
+# export processed_dir=${input_dir}/processed/train_peng_genia
+# export proto_dir=${processed_dir}/protos
+# export max_len=500000
+# replace infrequent tokens with <UNK>
+# export min_count=5
+
 export PYTHONPATH=$PYTHONPATH:`pwd`
 export CDR_IE_ROOT=`pwd`
